3c44dd5362d28d5eed4611e39d22b2a5af1ba419
mathurinm <mathurin.massias@gmail.com>
2017-11-09 11:20:04 +0100

    Update of sigmas every epoch in blockhomo
    vs every f_gap epoch previously

--- shcl/__init__.py
+++ shcl/__init__.py
@@ -1,2 +1,3 @@
 from .multitask_fast import (multitask_generalized_solver,
-                             multitask_blockhomo_solver)
+                             multitask_blockhomo_solver,
+                             multitask_blockhomo_solver_clever)
--- shcl/multitask_fast.pyx
+++ shcl/multitask_fast.pyx
@@ -89,6 +89,30 @@ cdef double spectral_norm_group(double[::1, :] X, double[::1, :] Sigma_inv_X,
 @cython.wraparound(False)
 @cython.boundscheck(False)
 @cython.cdivision(True)
+cdef double spectral_norm_group_clever(double[::1, :] X, double[:] sigmas_inv,
+                                int[:] block_indices, int[:] block_sizes,
+                                int g, double[::1, :] XgSigma_invXg,
+                                double[:] L_const_eigvals,
+                                double[:] L_const_WORK, int n_orient):
+    cdef int n_blocks = sigmas_inv.shape[0]
+    cdef int ii, jj, k
+    cdef int inc = 1
+    for ii in range(n_orient):
+        for jj in range(n_orient):
+            XgSigma_invXg[ii, jj] = 0.
+            for k in range(n_blocks):
+                XgSigma_invXg[ii, jj] += \
+                    ddot(&block_sizes[k], &X[block_indices[k], g + ii], &inc,
+                         &X[block_indices[k], g + jj], &inc) * sigmas_inv[k]
+
+    # perform eigval dec of XgSigma_invXg
+    my_eigh(XgSigma_invXg, L_const_eigvals, n_orient, L_const_WORK)
+    return L_const_eigvals[n_orient - 1]
+
+
+@cython.wraparound(False)
+@cython.boundscheck(False)
+@cython.cdivision(True)
 cdef BST(double * x, int x_len, double tau, double * zeros_like_x,
          int n_orient):
     """
@@ -112,7 +136,7 @@ cdef BST(double * x, int x_len, double tau, double * zeros_like_x,
         dcopy(&tmpint, zeros_like_x, &inc, x, &inc)
     else:
         dscal(&tmpint, &shrink, x, &inc)
-    # np.testing.assert_allclose()
+
 
 @cython.cdivision(True)
 @cython.boundscheck(False)
@@ -662,5 +686,267 @@ cpdef multitask_blockhomo_solver(double[::1, :] X, double[::1, :] Y, double alph
                              &inc, &diff_Beta_g[o, 0], &inc, &Sigma_inv_R[0, 0],
                              &n_samples)
                         break
+
+    return np.array(Beta), np.array(sigmas_inv)
+
+
+@cython.cdivision(True)
+@cython.boundscheck(False)
+cpdef multitask_blockhomo_solver_clever(double[::1, :] X, double[::1, :] Y,
+                            double alpha,
+                            double[:, ::1] Beta_init,
+                            int[:] block_indices,
+                            double tol=1e-4,
+                            long max_iter=10**5,
+                            int f_gap=10,
+                            double sigma_min=1e-3, verbose=1,
+                            int n_orient=1):
+    """Solve the optimization problem of the multi-task Block Homoscedastic
+    Smoothed Concomitant Lasso with alternate minimization.
+    We minimise in Beta and Sigma:
+    (1/(2 n_samples n_tasks) trace((Y - X Beta).T Sigma^-1 (Y - X Beta)) +
+    trace(Sigma) / (2 n_samples) + alpha sum(norm(Beta, ord=2, axis=1))
+
+    with the constraints that Sigma is diagonal, constant over blocks.
+
+    Parameters:
+    -----------
+        X : Fortran ndarray, shape (n_samples, n_features)
+            Training data.
+        Y : Fortran ndarray, shape (n_samples, n_tasks)
+            Target values.
+        alpha : float
+            Regularization parameter.
+        Beta_init : ndarray, shape (n_features, n_tasks)
+            Initialization of the regression coefficients.
+        block_indices : ndarray, shape (n_blocks + 1,)
+            The k-th block starts at sample block_indices[k] and stops at
+            sample block_indices[k + 1] - 1.
+        tol : float
+            The solver stops when reaching a duality gap lower than tol.
+        max_iter : int
+            If the duality gap does not go below tol after max_iter epochs of
+            coordinate descent, the solver stops.
+        sigma_min : float
+            Lower constraint on the noise levels per block.
+        verbose : {0, 1}
+            Level of verbosity.
+        n_orient : {1, 3}
+            Use 1 except when dealing with M/EEG applications, in which case
+            1 is for fixed orientation and 3 for free orientation.
+
+    Returns:
+    --------
+        Beta : ndarray, shape (n_features, n_tasks)
+            Estimated regression coefficients after optimization.
+        sigma_invs : ndarray, shape (n_blocks)
+            Inverses of estimated noise levels per block after optimization.
+    """
+    cdef int n_samples = X.shape[0]
+    cdef int n_features = X.shape[1]
+    cdef int n_tasks = Y.shape[1]
+    cdef int n_groups = n_features // n_orient
+    cdef int n_blocks = block_indices.shape[0] - 1
+    cdef int[:] block_sizes = np.diff(block_indices)
+
+    cdef double[:] sigmas = np.empty(n_blocks)
+    cdef double[:] sigmas_inv = np.empty(n_blocks)
+
+    cdef int it  # iterations
+    cdef int k  # blocks
+    cdef int i  # samples
+    cdef int j  # features
+    cdef int g  # groups of features. if n_orient == 1, g and j are the same.
+    cdef int o  # orientations
+    cdef int t  # tasks
+    cdef int inc = 1
+    cdef double zero = 0.
+    cdef double one = 1.
+    cdef double minus_one = - 1.
+    cdef int tmpint
+    cdef double tmpdouble
+    cdef double dual_norm_XtTheta
+    cdef double[:] norms_Theta_block = np.empty(n_blocks) # norm of block of Theta
+
+
+    cdef double[:] L_const = np.empty(n_groups)
+    cdef double[:] L_const_test = L_const.copy()
+    cdef double[:, ::1] zeros_like_Beta_g = np.zeros([n_orient, n_tasks])
+    # TODO E D as lists ?
+    cdef double[::1, :] R = np.empty([n_samples, n_tasks], order='F')
+
+    tmpint = n_features * n_tasks
+    cdef double[:, :] Beta = np.empty([n_features, n_tasks])
+    dcopy(&tmpint, &Beta_init[0, 0], &inc, &Beta[0, 0], &inc)
+
+    cdef double[::1, :] Theta = np.zeros([n_samples, n_tasks], order='F')
+    cdef double[:, ::1] XgTheta = np.zeros([n_orient, n_tasks])
+    cdef double[:, ::1] diff_Beta_g = np.zeros([n_orient, n_tasks])
+
+    # to compute spectral norms when n_orient != 1:
+    cdef double[::1, :] XjSigma_invXj = np.zeros([n_orient, n_orient], order='F')
+    cdef double[:] L_const_eigvals = np.zeros(n_orient)
+    cdef double[:] L_const_WORK  = np.empty(3 * n_orient - 1)
+
+    cdef double p_obj
+    cdef double d_obj
+    cdef double gap
+    cdef double scal
+
+    # cdef double[::1, :] R_test = np.asfortranarray(Y - np.dot(X, Beta))
+    # residuals initialization:
+    # R = Y.copy()
+    tmpint = n_samples * n_tasks
+    dcopy(&tmpint, &Y[0, 0], &inc, &R[0, 0], &inc)
+    # R -= np.dot(X, Beta)
+    for j in range(n_features):
+        for t in range(n_tasks):
+            if Beta[j, t] != 0.:
+                dger(&n_samples, &n_tasks, &minus_one, &X[0, j],
+                     &inc,
+                    &Beta[j, 0], &inc, &R[0, 0], &n_samples)
+                break
+
+    for it in range(max_iter):
+        if True:
+            for k in range(n_blocks):
+                sigmas[k] = 0.
+
+                for t in range(n_tasks):
+                    sigmas[k] += dnrm2(
+                        &block_sizes[k],  &R[block_indices[k], t], &inc) ** 2
+
+                sigmas[k] = sqrt(sigmas[k] / (n_tasks * block_sizes[k]))
+
+                if sigmas[k] < sigma_min:
+                    sigmas[k] = sigma_min
+                sigmas_inv[k] = 1. / sigmas[k]
+
+            for g in range(n_groups):
+                L_const[g] = 0.
+                if n_orient == 1:
+                    for k in range(n_blocks):
+                        L_const[g] += dnrm2(&block_sizes[k],
+                                            &X[block_indices[k], g],
+                                            &inc) ** 2 * sigmas_inv[k]
+                else:
+                    L_const[g] = spectral_norm_group_clever(
+                        X, sigmas_inv, block_indices, block_sizes,
+                        g * n_orient, XjSigma_invXj, L_const_eigvals,
+                        L_const_WORK, n_orient)
+
+
+        if it % f_gap == 0:
+            p_obj = 0.
+            # quadratic data-fitting term:
+            for t in range(n_tasks):
+                for k in range(n_blocks):
+                    p_obj += dnrm2(&block_sizes[k], &R[block_indices[k], t],
+                                   &inc) ** 2 * sigmas_inv[k]
+            p_obj /= 2. * n_samples * n_tasks
+            # sigma penalization:
+            for k in range(n_blocks):
+                p_obj += block_sizes[k] * sigmas[k] / (2 * n_samples)
+            # beta penalization:
+            tmpint = n_tasks * n_orient
+            for g in range(n_groups):
+                p_obj += dnrm2(&tmpint, &Beta[g * n_orient, 0], &inc) * alpha
+
+            # Theta = Sigma^-1 * R / (n_tasks * n_samples * alpha)
+            tmpint = n_samples * n_tasks
+            dcopy(&tmpint, &R[0, 0], &inc, &Theta[0, 0], &inc)
+            for t in range(n_tasks):
+                for k in range(n_blocks):
+                    tmpdouble = 1. / (n_tasks * n_samples * alpha * sigmas[k])
+                    dscal(&block_sizes[k], &tmpdouble,
+                          &Theta[block_indices[k], t], &inc)
+
+            dual_norm_XtTheta = 0.
+            tmpint = n_tasks * n_orient  # number of elements in XgTheta
+            for g in range(n_groups):
+                for t in range(n_tasks):
+                    for o in range(n_orient):
+                        XgTheta[o, t] = \
+                            ddot(&n_samples, &X[0, g * n_orient + o], &inc,
+                                 &Theta[0, t], &inc)
+                dual_norm_XtTheta = fmax(dual_norm_XtTheta,
+                                         dnrm2(&tmpint, &XgTheta[0, 0], &inc))
+
+            for k in range(n_blocks):
+                norms_Theta_block[k] = 0.
+
+                for t in range(n_tasks):
+                    norms_Theta_block[k] += dnrm2(&block_sizes[k],
+                                              &Theta[block_indices[k], t],
+                                              &inc) ** 2
+
+                norms_Theta_block[k] = sqrt(norms_Theta_block[k])
+
+            # dual point will be Theta /scal
+            scal = 0.
+            for k in range(n_blocks):
+                scal = fmax(scal, fmax(1, norms_Theta_block[k] * \
+                    sqrt(n_tasks / block_sizes[k]) * n_samples * alpha))
+
+            scal = fmax(dual_norm_XtTheta, scal)
+
+            if scal > 1.:
+                tmpdouble = 1. / scal
+                tmpint = n_tasks * n_samples
+                dscal(&tmpint, &tmpdouble, &Theta[0, 0], &inc)
+
+            tmpint = n_tasks * n_samples
+            d_obj = alpha * ddot(&tmpint, &Y[0, 0], &inc, &Theta[0, 0], &inc)
+            for k in range(n_blocks):
+                d_obj += sigma_min * (block_sizes[k] - (n_samples * sqrt(n_tasks) *
+                         alpha * norms_Theta_block[k]) ** 2) / (2 * n_samples)
+
+            if verbose:
+                iter_string = "Iteration %d" % it
+                print("%s, p_obj=%.8f" % (iter_string, p_obj))
+                print("%s  d_obj=%.8f" % (" " * len(iter_string), d_obj))
+
+            gap = p_obj - d_obj
+            if gap < tol:
+                print("Early exit, gap: %s" % format(gap))
+                break
+
+            # np.testing.assert_allclose(np.array(Sigma_inv_R),
+            #     np.dot(Sigma_inv, Y - np.dot(X, Beta)))
+
+        tmpint = n_orient * n_tasks
+        for g in range(n_groups):
+            dcopy(&tmpint, &Beta[g * n_orient, 0], &inc,
+                  &diff_Beta_g[0, 0], &inc)
+
+            for o in range(n_orient):
+                for t in range(n_tasks):
+                    for k in range(n_blocks):
+                        Beta[g * n_orient + o, t] += ddot(
+                            &block_sizes[k], &X[block_indices[k], g * n_orient + o],
+                            &inc, &R[block_indices[k], t],
+                            &inc) / L_const[g] * sigmas_inv[k]
+
+            # in place soft thresholding
+            tmpdouble = alpha * n_samples * n_tasks / L_const[g]
+            BST(&Beta[g * n_orient, 0], n_tasks, tmpdouble,
+                &zeros_like_Beta_g[0, 0], n_orient)
+
+
+            # diff_Beta_g -= new value of Beta g
+            daxpy(&tmpint, &minus_one, &Beta[g * n_orient, 0], &inc,
+                  &diff_Beta_g[0, 0], &inc)
+
+            # keep residuals up to date:
+            # (do it only if update is non-zero)
+            for o in range(n_orient):
+                for t in range(n_tasks):
+                    if diff_Beta_g[o, t] != 0.:
+                        dger(&n_samples, &n_tasks, &one,
+                             &X[0, g * n_orient + o],
+                             &inc, &diff_Beta_g[o, 0], &inc, &R[0, 0],
+                             &n_samples)
+                        break
+
 
     return np.array(Beta), np.array(sigmas_inv)
